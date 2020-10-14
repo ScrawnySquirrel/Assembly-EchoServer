1629c7e786cf03d901b5ad60fa63421defcd51d9
Gabriel Lee <gabriellee@Gabriels-MacBook-Air.local>
2020-10-13 22:59:41 -0700

    Add communication to server functionality

--- client.asm
+++ client.asm
@@ -17,33 +17,38 @@ endstruc
 
 section .bss
     sock resw 2
-    client resw 2
+    server resw 2
     echobuf resb 256
     read_count resw 2
 
-    ipaddr resw 2
+    ipaddr resd 2
+    pStruc resd 1
 
 section .data
     sock_err_msg        db "Failed to initialize socket", 0x0a, 0
     sock_err_msg_len    equ $ - sock_err_msg
 
-    bind_err_msg        db "Failed to bind socket", 0x0a, 0
-    bind_err_msg_len    equ $ - bind_err_msg
-
-    lstn_err_msg        db "Socket Listen Failed", 0x0a, 0
-    lstn_err_msg_len    equ $ - lstn_err_msg
-
-    accept_err_msg      db "Accept Failed", 0x0a, 0
-    accept_err_msg_len  equ $ - accept_err_msg
+    connect_err_msg      db "Connect Failed", 0x0a, 0
+    connect_err_msg_len  equ $ - connect_err_msg
 
     accept_msg          db "Client Connected!", 0x0a, 0
     accept_msg_len      equ $ - accept_msg
+
+    ip_msg          db "IP:", 0x0a, 0
+    ip_msg_len      equ $ - ip_msg
+
+    client_prompt          db "Client message:", 0x0a, 0
+    client_prompt_len      equ $ - client_prompt
+
+    server_prompt          db "Server message:", 0x0a, 0
+    server_prompt_len      equ $ - server_prompt
+
 
     ;; sockaddr_in structure for the address the listening socket binds to
     pop_sa istruc sockaddr_in
         at sockaddr_in.sin_family, dw 2           ; AF_INET
         at sockaddr_in.sin_port, dw 0xce56        ; port 22222 in host byte order
-        at sockaddr_in.sin_addr, dd 0             ; localhost - INADDR_ANY
+        at sockaddr_in.sin_addr, dd 0x4501A8C0             ; localhost - INADDR_ANY
         at sockaddr_in.sin_zero, dd 0, 0
     iend
     sockaddr_in_len     equ $ - pop_sa
@@ -54,25 +59,24 @@ section .text
 _start:
     ;; Initialize listening and client socket values to 0, used for cleanup
     mov      word [sock], 0
-    mov      word [client], 0
+    mov      word [server], 0
 
     ;; Initialize socket
     call     _socket
 
-    ;; Bind and Listen
-    call     _listen
-
+    ;; Get IP addr from user
     call _get_ip
 
     ;; Main loop handles connection requests (accept()) then echoes data back to client
     .mainloop:
-        call     _accept
+        call     _connect
 
         ;; Read and echo string back to the client
         ;; up the connection on their end.
         .readloop:
-            call     _read
+            call _get_msg
             call     _echo
+            call _read
 
             ;; read_count is set to zero when client hangs up
             mov     rax, [read_count]
@@ -82,9 +86,9 @@ _start:
 
         .read_complete:
         ;; Close client socket
-        mov    rdi, [client]
+        mov    rdi, [server]
         call   _close_sock
-        mov    word [client], 0
+        mov    word [server], 0
     jmp    .mainloop
 
     ;; Exit with success (return 0)
@@ -109,44 +113,20 @@ _socket:
 
     ret
 
-;; Calls sys_bind and sys_listen to start listening for connections
-_listen:
-    mov        rax, 49                  ; SYS_BIND
-    mov        rdi, [sock]              ; listening socket fd
-    mov        rsi, pop_sa              ; sockaddr_in struct
-    mov        rdx, sockaddr_in_len     ; length of sockaddr_in
-    syscall
-
-    ;; Check call succeeded
-    cmp        rax, 0
-    jl         _bind_fail
-
-    ;; Bind succeeded, call sys_listen
-    mov        rax, 50          ; SYS_LISTEN
-    mov        rsi, 5           ; backlog
-    syscall
-
-    ;; Check for success
-    cmp        rax, 0
-    jl         _listen_fail
-
-    ret
-
 ;; Accept a cleint connection and store the new client socket descriptor
-_accept:
-    ;; Call sys_accept
-    mov       rax, 43         ; SYS_ACCEPT
-    mov       rdi, [sock]     ; listening socket fd
-    mov       rsi, 0          ; NULL sockaddr_in value as we don't need that data
-    mov       rdx, 0          ; NULLs have length 0
+_connect:
+    mov rax, 42 ; SYS_CONNECT – 64-bit
+    mov rdi, [sock] ; listening socket fd
+    mov rsi, pop_sa ; pop_sa would contain the addressing information
+    mov rdx, sockaddr_in_len ; socklen_t
     syscall
 
     ;; Check if call succeeded
     cmp       rax, 0
-    jl        _accept_fail
+    jl        _connect_fail
 
     ;; Store returned client socket descriptor
-    mov     [client], rax
+    mov     [server], rax
 
     ;; Print connection message to stdout
     mov       rax, 1             ; SYS_WRITE
@@ -162,13 +142,23 @@ _accept:
 _read:
     ;; Call sys_read
     mov     rax, 0          ; SYS_READ
-    mov     rdi, [client]   ; client socket fd
+    mov     rdi, [sock]   ; client socket fd
     mov     rsi, echobuf    ; buffer
     mov     rdx, 256        ; read 256 bytes
     syscall
 
-    ;; Copy number of bytes read to variable
-    mov     [read_count], rax
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, server_prompt
+    mov edx, server_prompt_len
+    int 80h
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, echobuf
+    mov edx, 256
+    int 80h
 
     ret
 
@@ -176,10 +166,13 @@ _read:
 ;; using sys_write
 _echo:
     mov     rax, 1               ; SYS_WRITE
-    mov     rdi, [client]        ; client socket fd
+    mov     rdi, [sock]        ; client socket fd
     mov     rsi, echobuf         ; buffer
-    mov     rdx, [read_count]    ; number of bytes received in _read
+    mov     rdx, 256    ; number of bytes received in _read
     syscall
+
+    ;; Copy number of bytes read to variable
+    mov     [read_count], rax
 
     ret
 
@@ -199,19 +192,9 @@ _socket_fail:
     mov     rdx, sock_err_msg_len
     call    _fail
 
-_bind_fail:
-    mov     rsi, bind_err_msg
-    mov     rdx, bind_err_msg_len
-    call    _fail
-
-_listen_fail:
-    mov     rsi, lstn_err_msg
-    mov     rdx, lstn_err_msg_len
-    call    _fail
-
-_accept_fail:
-    mov     rsi, accept_err_msg
-    mov     rdx, accept_err_msg_len
+_connect_fail:
+    mov     rsi, connect_err_msg
+    mov     rdx, connect_err_msg_len
     call    _fail
 
 ;; Calls the sys_write syscall, writing an error message to stderr, then exits
@@ -230,15 +213,15 @@ _fail:
 _exit:
     mov        rax, [sock]
     cmp        rax, 0
-    je         .client_check
+    je         .server_check
     mov        rdi, [sock]
     call       _close_sock
 
-    .client_check:
-    mov        rax, [client]
+    .server_check:
+    mov        rax, [server]
     cmp        rax, 0
     je         .perform_exit
-    mov        rdi, [client]
+    mov        rdi, [server]
     call       _close_sock
 
     .perform_exit:
@@ -246,16 +229,50 @@ _exit:
     syscall
 
 _get_ip:
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, ip_msg
+    mov edx, ip_msg_len
+    int 80h
+
     mov eax, 3
     mov ebx, 0
     mov ecx, ipaddr
     mov edx, 256
     int 80h
 
+    ; mov eax, 4
+    ; mov ebx, 1
+    ; mov ecx, ipaddr
+    ; mov edx, 256
+    ; int 80h
+
+    ret
+
+
+_get_msg:
     mov eax, 4
     mov ebx, 1
-    mov ecx, ipaddr
+    mov ecx, client_prompt
+    mov edx, client_prompt_len
+    int 80h
+
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, echobuf
     mov edx, 256
     int 80h
 
+    ; mov eax, 4
+    ; mov ebx, 1
+    ; mov ecx, ipaddr
+    ; mov edx, 256
+    ; int 80h
+
     ret
+
+_print:
+    mov eax, 4
+    mov ebx, 1
+    mov edx, 256
+    int 80h
